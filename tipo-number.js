5b1d1bd0f4ed1d139575618f096eedaa20d785ce
Colyss-io <168148848+Colyss-io@users.noreply.github.com>
2024-04-26 19:27:43 -0300

    tipos de números

---
+++ tipo-number.js
@@ -0,0 +1,10 @@
+const notaPrimeiroBi = 8
+const notaSegundoBi = 6.3
+const notaTerceiroBi = -2
+const notaQuartoBi = Number.parseInt ('5')
+
+const total = (notaPrimeiroBi + notaSegundoBi + notaTerceiroBi + notaQuartoBi) /4
+console.log('a média é' + total.toFixed(2))
+
+const nome = 'Gabriel'
+console.log('Meu nome é ' + nome)
