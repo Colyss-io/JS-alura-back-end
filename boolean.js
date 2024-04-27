b47bc7f1279f7e3151ba2a8ca03f0b43044701e0
Colyss-io <168148848+Colyss-io@users.noreply.github.com>
2024-04-27 14:09:23 -0300

    boolean

---
+++ boolean.js
@@ -0,0 +1,16 @@
+const estudante = 'Fernando'
+const estaAprovado = true
+const naoEstaAprovado = false
+
+if(estaAprovado) {
+    console.log('parabéns, boas festas')
+} else {
+    console.log('Reprovado')
+}
+
+if (estudante === 'Fernando'){
+     console.log(`olá, ${estudante}`)
+} else {
+    console.log(`reprovado, ${estudante}`)
+
+}
