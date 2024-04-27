06d762148cf02367b4eaf1de59798853d5141ed2
Colyss-io <168148848+Colyss-io@users.noreply.github.com>
2024-04-27 17:25:56 -0300

    array desafios

---
+++ atualizar-lista.js
@@ -0,0 +1,6 @@
+const listaEstudantes = ['João', 'Ana', 'Caio', 'Lara', 'Marjure']
+
+listaEstudantes.splice(1, 2, 'Rodrigo')
+
+
+console.log(listaEstudantes)
---
+++ juntar-salas.js
@@ -0,0 +1,5 @@
+const salaJS = ['Evaldo', 'Camis', 'Mari']
+const salaPython = ['ju', 'Leo', 'Raquel']
+
+const salasUnificadas = salaJS.concat(salaPython)
+console.log(salasUnificadas)
