fea5ab877f55d815d5c2a0a17a791cb2c722221f
Colyss-io <168148848+Colyss-io@users.noreply.github.com>
2024-04-27 14:28:25 -0300

    operadores

---
+++ null-undefined.js
@@ -0,0 +1,15 @@
+
+//ambos são tipos primitivos
+let nomeEstudante
+const professora = 'Ana'
+
+// typeof alertar qual o tipo da variável
+console.log(nomeEstudante)
+console.log(typeof nomeEstudante)
+console.log(typeof professora)
+
+// declarar como nula
+let telefoneEstudante = null
+
+//apresentar ela dizendo qual o nome
+console.log(typeof telefoneEstudante)
